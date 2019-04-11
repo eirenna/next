63e5c890cfc448928cfe71f7acab0120b2600679
eirenna <rybalchenko.iryna@gmail.com>
2019-04-11 15:54:35 +0300

    init2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
-;(function() {
+;
+(function () {
 	"use strict";
-
+	$(document).ready(function () {
+		$('.ba-portfolio').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			infinite: true,
+			autoplay: true,
+			arrows: false,
+			centerMode: true,
+			variableWidth: true,
+			mobileFirst: true,
+			responsive: [
+				{
+					breakpoint: 1905,
+					settings: 'unslick'
+				}
+			]
+		});
+	});
+	let btn = document.querySelector('.ba-menu-toggle');
+      let menu = document.querySelector('.ba-menu');
+      btn.addEventListener('click', function (e) {
+      	menu.classList.toggle('open');
+	  });
+	  
+	  $(window).scroll(function(){
+		if ($(window).scrollTop() > 200) {
+			$('.ba-header').addClass('ba-scroll');
+		}
+		else {
+			$('.ba-header').removeClass('ba-scroll');
+		}
+	});  
 })();
-
